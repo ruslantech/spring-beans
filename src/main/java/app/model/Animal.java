aee9a00752a8b3ec103d87b1893234c515bb190d
ruslantech <ruslantech1@gmail.com>
2021-02-04 17:07:45 +0600

    test

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,9 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("dog")
 public abstract class Animal {
 }
